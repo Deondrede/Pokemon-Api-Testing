975d7d37e3acc443126405062196fa4509653aa1
Deondre De Freitas <deondrede@gmail.com>
2019-08-31 22:23:16 -0400

    Added CSS styling and HTML to be appended

--- pokeSubmit.js
+++ pokeSubmit.js
@@ -34,32 +34,32 @@ function pokeSubmit(){  //v1 of the pokeApi is deprecated, use v2
         //Stats
         for (i in data.stats){
             if (data.stats[i].stat.name == "hp"){
-                document.getElementById("hp").value = data.stats[i].base_stat;
+               var statHP = data.stats[i].base_stat;
             }
         }
         for (i in data.stats){
             if (data.stats[i].stat.name == "defense"){
-                document.getElementById("defense").value = data.stats[i].base_stat;
+                var statDefense = data.stats[i].base_stat;
             }
         }
         for (i in data.stats){
             if (data.stats[i].stat.name == "attack"){
-                document.getElementById("attack").value = data.stats[i].base_stat;
+                var statAttack = data.stats[i].base_stat;
             }
         }
         for (i in data.stats){
             if (data.stats[i].stat.name == "special-attack"){
-                document.getElementById("sp.attack").value = data.stats[i].base_stat;
+                var statSpAttack = data.stats[i].base_stat;
             }
         }
         for (i in data.stats){
             if (data.stats[i].stat.name == "special-defense"){
-                document.getElementById("sp.defense").value = data.stats[i].base_stat;
+                var statSpDefense = data.stats[i].base_stat;
             }
         }
         for (i in data.stats){
             if (data.stats[i].stat.name == "speed"){
-                document.getElementById("speed").value = data.stats[i].base_stat;
+                var statSpeed = data.stats[i].base_stat;
             }
         }
 
@@ -82,8 +82,21 @@ function pokeSubmit(){  //v1 of the pokeApi is deprecated, use v2
                 }
             }
              // append data to HTML
-            // empty string to hold HTML
             var html = "";
+            html += '<div class="Stats" style="font-size: 15px;">';
+            html += '<p>HP</p>';
+            html += '<progress id="hp" max="255" value="' + statHP + '"></progress>';
+            html += '<p>Attack</p>';
+            html += '<progress id="attack" max="180" value="' + statAttack + '"+></progress>';
+            html += '<p>Sp.Attack</p>';
+            html += '<progress id="sp.attack" max="180" value="' + statSpAttack + '"></progress>';
+            html += '<p>Defense</p>';
+            html += '<progress id="defense" max="230" value="' + statDefense + '"></progress>';
+            html += '<p>Sp.Defense</p>';
+            html += '<progress id="sp.defense" max="230" value="' + statSpDefense + '"></progress>';
+            html += '<p>Speed</p>';
+            html += '<progress id="speed" max="180" value="' + statSpeed + '"></progress>';
+            html += '</div>';
             html += '<img src="' + imageURI + '">';
             html += '<h1>#' + pokeID + ' ' + pokeName + '</h1>';
             html += '<h3>' + pokeGenus + '</h3>';
@@ -93,8 +106,9 @@ function pokeSubmit(){  //v1 of the pokeApi is deprecated, use v2
             else {
                 html += '<p>Type: ' + pokeType1 + '</p>'; // only display Type 2 if it is not null
             }
-            
+            html += '<div class="boxed-descrption" style="border-radius: 15px 50px 30px 0px;background: transparent;border: 5px solid black;padding: 5px;width: 60%;height: auto; display: inline-block;">'
             html += '<p>' + pokeDescription + '</p>';
+            html += '</div>';
 
             // empty the listview
             $("#pokeDetails").empty();
